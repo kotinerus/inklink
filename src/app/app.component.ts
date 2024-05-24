d91b296d9c8ee8cc16bc08cc39e89eae72e108da
Krzysztof Wysocki <krzysztof.wysocki@dsr.com.pl>
2024-05-24 14:47:22 +0200

    Zmiany

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatIconModule } from '@angular/material/icon';
+
 import { MatDividerModule } from '@angular/material/divider';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, MatButtonModule, MatDividerModule, MatIconModule, MatButtonToggleModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatDividerModule, MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'materialNauka';
+  tiles: Tile[] = [
+    { text: 'One', cols: 4, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 1, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'User', cols: 1, rows: 1, color: 'lightpink' },
+  ];
 }
